cac610ef84ce42d24e5fd5280e425edeee971829
Petryck WIlliam <petryck@i9devgroup.com>
2022-04-04 13:36:53 -0300

    .
    .

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,23 @@
-const path = require('path')
+// const path = require('path')
+
+// module.exports = {
+//   mode: 'development',
+//   devtool: 'eval-cheap-source-map',
+//   entry: './public/assets/js/controller.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, '../public/assets/js')
+//   }
+// }
+
+
+const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
   entry: './public/assets/js/controller.js',
+  devtool: 'eval-cheap-source-map',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../public/assets/js')
-  }
-}
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/assets/js'),
+  },
+};
